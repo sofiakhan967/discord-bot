edee648c139336ca26deb923ed999cd77e47dd5e
sofia <khansofia967@gmail.com>
2021-05-25 18:04:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from discord import message
+import random
+from discord.ext import commands
+
+token=('ODQxNDE4ODM1NzA1NzI0OTQ4.YJmeWw.ymMketqJuLSGWrwuOdJ4Bo3XYbE')
+
+words=['hi','hello','whatsup','good to see you']
+reply=['hi', 'hello' , 'good to see you too', 'hey buddy', 'much better','its been a long day ']
+intents=discord.Intents.default()
+intents.members=True
+#intents=intents
+client=commands.Bot(command_prefix="." ,intents=intents)
+@client.event
+async def on_ready():
+    print(f"{client.user} has been connected succesfully")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('hi')
+
+@client.event
+async def on_member_join(member):
+    guild=client.get_guild(841419937256046613) #server id
+    channel=guild.get_channel(841419937708245024) #channel id
+    await channel.send(f'{member.mention} welcome to server ! :partying_face:') #welcome member on server
+    await member.send(f'welcome to {guild.name} server , {member.mention} , :partying_face:') #welcome msg DM
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if any (word in message.content for word in words):
+        await message.channel.send(random.choice(reply))
+client.run(token)    
